f293647c68ed1bf381d4f8702e4bd58a94d81718
cookie_jar <cptpricecod098@gmail.com>
2024-12-27 20:36:15 +0530

    Database connections fixed

--- database/database.go
+++ database/database.go
@@ -9,12 +9,14 @@ import (
 
 var DB *gorm.DB
 
-func Connect() {
+func Connect() (*gorm.DB, error) {
 	var err error
 	DB, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err)
+		return nil, err
 	}
 
 	log.Println("Database connection established.")
+	return DB, nil
 }
--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/dibyajyoti-mandal/cli-backend/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func setupRoutes(app *fiber.App) {
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Api test")
+	})
 	// app.Get("/api/getItems", GetItems)
 	// app.Get("/api/getItem", GetItem)
 	// app.Post("/api/addItem", AddItem)
@@ -15,14 +20,32 @@ func setupRoutes(app *fiber.App) {
 	fmt.Println("Routes setup...")
 }
 
-func initDB() {
-
-}
-
 func main() {
-	fmt.Print("BOOTING BACKEND...\n")
+	fmt.Print("BOOTING SERVER...\n")
+
+	// Initialize Fiber app
 	app := fiber.New()
+
+	// Set up routes
 	setupRoutes(app)
-	database.Connect()
-	app.Listen(":8000")
+
+	// Connect to db
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	// Closing db
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get sql.DB from gorm.DB: %v", err)
+	}
+	defer func() {
+		if err := sqlDB.Close(); err != nil {
+			log.Fatalf("Failed to close database connection: %v", err)
+		}
+	}()
+	// Start the server
+	if err := app.Listen(":8000"); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
